109c0ce8ae650d8d76a9b6469c55eedb68928d19
Luiz  Daniel <luiz.kdanimaster@gmail.com>
2021-11-29 14:21:23 -0300

    base para exercicio 17 grupo 3

--- src/grupo03/Grupo03Ex17.java
+++ src/grupo03/Grupo03Ex17.java
@@ -1,137 +1,85 @@
 package grupo03;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-//47.	Leia uma relação de pacientes de uma clínica, cada um com o nome, o sexo, o peso, 
-//a idade e a altura.  Para sinalizar o fim da lista será fornecido “fim” no nome do último
+//47.	Leia uma relaï¿½ï¿½o de pacientes de uma clï¿½nica, cada um com
+// o nome, o sexo, o peso,
+// a idade e a altura.
+// Para sinalizar o fim da lista serï¿½ fornecido ï¿½fimï¿½ no nome do ï¿½ltimo
 //paciente.
 
-//Exiba um relatório contendo:
+//Exiba um relatï¿½rio contendo:
 //i.    a quantidade de pacientes.
-//ii.   a média de idade dos homens.
+//ii.   a mï¿½dia de idade dos homens.
 //iii.  a quantidade de mulheres com altura entre 1,60 e 1,70 e peso acima de 70kg.
 //iv.  a quantidade de pessoas com idade entre 18 e 25.
-//v.   o nome do paciente mais velho. 
+//v.   o nome do paciente mais velho.
 //vi.  o nome da mulher mais baixa.
-//vii. – neste item, você cria uma situação interessante constrói o algoritmo correspondente.   
+//vii. ï¿½ neste item, vocï¿½ cria uma situaï¿½ï¿½o interessante constrï¿½i o algoritmo correspondente.   
 
 public class Grupo03Ex17 {
-	
 
 	public static void main (String[] args) {
-		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System. in);
-		
-		
-		int somaIdadeHomens = 0;
-		int somaAlturaEpeso = 0;
-		int somaIdade18A25 = 0;
-		int IdadeDoMaisVelho = 0;
-		
-		ArrayList <String> nomes = new ArrayList<String>();
-		ArrayList <String> sexo = new ArrayList<>();
-		ArrayList <Double> peso = new ArrayList<Double>();
-		ArrayList <Integer> idade = new ArrayList<Integer>();
-		ArrayList <Double> altura = new ArrayList<Double>();		
-		
-		for(int i = 0; i <= nomes.size(); i++ ) {		
-		
-			System.out.println("Digite o nome");	
-			String inicio = sc.next();			
-						
-			if(inicio.equals("fim")) {
-				
-			i = i + 1 ;
-				
-			}
-			else {
-				
-				nomes.add(i,inicio);	
-			
-				System.out.println("Digite o sexo");
-				String sexoInicio = sc.next();
-				sexo.add(i,sexoInicio);
-			
-				System.out.println("Digite o peso");
-				double pesoInicio = sc.nextDouble();
-				peso.add(i,pesoInicio);
-			
-				System.out.println("Digite a idade");
-				int idadeInicio = sc.nextInt();
-						
-				idade.add(i,idadeInicio);			
-			
-				System.out.println("Digite a altura");
-				double alturaInicio = sc.nextDouble();
-				altura.add(i,alturaInicio);			
-			
-				if(sexoInicio.equals("M")) {				
-					somaIdadeHomens = somaIdadeHomens + idadeInicio;
-					double mediaDeIdadeHomens = somaIdadeHomens / idade.size();	
-				
-				}
-			
-				
-			
-				if(idadeInicio > 18 && idadeInicio < 25) {
-					somaIdade18A25 = somaIdade18A25 + 1; 
-				
-				}			
-			}			
-		}
-		
-		
-		
-		for(int i = 0; i <= nomes.size(); i++ ) {				
-			
-				int comparador = idade.get(i);
-				
-				if(comparador > IdadeDoMaisVelho) {
-					IdadeDoMaisVelho = comparador;					
-				}				
-			}
-			
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("Quantidade de pacientes: " + sexo.size());
-		System.out.println("Média de idade dos homens: " + (somaIdadeHomens / idade.size()));
-		System.out.println("quantidade de mulheres com altura entre 1,60 e 1,70 e peso acima de 70kg: " );
-		System.out.println("Pessoas entre 18 e 25 anos: " + somaIdade18A25);
-		System.out.println("O nome do paciente mais velho " + nomes.indexOf(IdadeDoMaisVelho));
-		System.out.println(sexo.indexOf("F"));
-			
-		
-		
-		
-		
-		
-		sc.close();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		List<Paciente> pacientes = preencherListaPacientes();
+		exibeRelatorio(pacientes);
 	}
+
+	public static void exibeRelatorio(List<Paciente> pacientes){
+		System.out.println("RelÃ¡torio de pacientes da clinica: ");
+		System.out.println("Total de pacientes: " + pacientes.size());
+		System.out.println("Media de idade dos homens: " + calcularMediaIdadeHomens(pacientes));
+		//i.    a quantidade de pacientes.
+//ii.   a mï¿½dia de idade dos homens.
+//iii.  a quantidade de mulheres com altura entre 1,60 e 1,70 e peso acima de 70kg.
+//iv.  a quantidade de pessoas com idade entre 18 e 25.
+//v.   o nome do paciente mais velho.
+//vi.  o nome da mulher mais baixa.
+//vii. exibir o maior e o menor imc dos pacientes na lista.
+	}
+
+	public static List<Paciente> preencherListaPacientes(){
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System. in);
+		List<Paciente> pacientes = new ArrayList<>();
+
+		while(true){
+			Paciente paciente = new Paciente();
+			System.out.println("Digite o nome do paciente: ");
+			String nome = sc.next();
+			if(nome.equals("fim")) break;
+			paciente.nome = nome;
+			System.out.println("Digite o sexo do paciente (F - Feminino, M - Masculino): ");
+			paciente.sexo = sc.next();
+			System.out.println("Digite o peso do paciente: ");
+			paciente.peso = sc.nextDouble();
+			System.out.println("Digite a altura do paciente em centimetros: ");
+			paciente.altura = sc.nextInt();
+			System.out.println("Digite a idade do paciente: ");
+			paciente.idade = sc.nextInt();
+			pacientes.add(paciente);
+			System.out.println("Paciente " + nome + " adicionado a lista da clÃ­nica");
+		}
+		sc.close();
+		return pacientes;
+	}
+
+	public static double calcularMediaIdadeHomens(List<Paciente> pacientes){
+		double contadorHomens = 0.0;
+		double somaIdadeHomens = 0.0;
+		for (Paciente p : pacientes) {
+			if(p.sexo.equals("m")){
+				contadorHomens++;
+				somaIdadeHomens += p.idade;
+			}
+		}
+		return somaIdadeHomens/contadorHomens;
+	}
+
 
 }
---
+++ src/grupo03/Paciente.java
@@ -0,0 +1,18 @@
+ package grupo03;
+
+public class Paciente {
+
+  String nome;
+  String sexo;
+  Double peso;
+  Integer altura;
+  Integer idade;
+
+  public String toString(){
+    return "Paciente[nome: " + nome +
+        " sexo: " + sexo +
+        " peso: " + peso +
+        " altura: " + altura +
+        " idade: " + idade + "]";
+  }
+}
